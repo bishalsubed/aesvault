de819d4cf1ab6e6921441807d572dba3a67649fa
bishalsubed <142403372+bishalsubed@users.noreply.github.com>
2025-06-06 00:54:50 +0545

    updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,11 @@
+import { fileURLToPath } from 'url'
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// Needed to define __dirname in ESM context
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
