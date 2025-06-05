7464c272fd36b0d9a33c844891f13d9ec5bd7b03
bishalsubed <142403372+bishalsubed@users.noreply.github.com>
2025-06-06 02:05:34 +0545

    updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
-const { fileURLToPath } = require('url')
-const path = require('path')
-const { defineConfig } = require('vite')
-const react = require('@vitejs/plugin-react')
+/// <reference types="node" />
+/// <reference types="vite/client" />
 
-// Needed to define __dirname in ESM context
+import * as path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Define __dirname for ESM context
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -21,11 +23,11 @@ export default defineConfig({
         target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
   build: {
-    outDir: "dist"
+    outDir: "dist",
   },
-  base: "/"
+  base: "/",
 })
